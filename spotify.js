fc2dac9567965192fa175ae66ba439a799531893
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2023-07-08 14:47:43 -0400

    Create spotify.js

---
+++ spotify.js
@@ -0,0 +1,12 @@
+const playlistId = '6GGEimRQbfKBA3QQ8IPk6Q';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/6GGEimRQbfKBA3QQ8IPk6Q?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
